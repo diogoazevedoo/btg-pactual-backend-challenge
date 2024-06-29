14d6a72350c7e9554135ae8305c6b0dafbe52f00
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-06-29 19:41:52 +0100

    feat: add rabbitmq config

---
+++ src/main/java/com/diogoazevedo/btgpactual/orderms/config/RabbitMQConfig.java
@@ -0,0 +1,8 @@
+package com.diogoazevedo.btgpactual.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
